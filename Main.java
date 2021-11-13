1808a705d3073fd4fa5d96cde07175d93f13b406
ajaniSterling <ajani.sterling@hgs.hiddengeniusproject.org>
2021-11-13 01:33:13 +0000

    Variables

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String [] args) 
+    {
+    
+
+      String name = "Ajani Sterling";
+      String timeManagement = "setting reminders for myself";
+      String personalCommitment = "things that I have set up for myself";
+      String KPIMetrics = "how much work I have done";
+
+      System.out.println("Hello, My name is " + name +", the time management stratagy that I would like to be held accountable to is " + timeManagement + ", I will personally commit to " + personalCommitment + ", " + "My buisness' main KPI is/are " + KPIMetrics + "."); 
+      }
+}
+
+
